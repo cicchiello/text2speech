bc723fc4d3b52897613622d6d514595942765e5c
jcicchiello-at-888270522287 <j.cicchiello@ieee.org>
2023-12-05 16:49:50 -0500

    - ported to latest SDK and gradle and java - added second default speech string

--- app/src/main/java/com/jfc/text2speech/Text2Speech.java
+++ app/src/main/java/com/jfc/text2speech/Text2Speech.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,45 +22,11 @@ public class Text2Speech extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_margaritaville);
 
-        final EditText et = (EditText) findViewById(R.id.theText);
+        Button b1 = findViewById(R.id.speak_button1);
+        b1.setOnClickListener(v -> buttonClickImp(findViewById(R.id.theText1)));
 
-        Button b;
-        b = (Button) findViewById(R.id.speak_button);
-        if (b != null) {
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String t = et.getText().toString();
-                    if ((t == null) || t.equals(""))
-                        t = et.getHint().toString();
-                    if ((t != null) && !t.equals("")) {
-
-                        final String finalText = t;
-                        mTts = new TextToSpeech(mActivity, new TextToSpeech.OnInitListener() {
-
-                            @Override
-                            public void onInit(int status) {
-                                // TODO Auto-generated method stub
-                                if(status == TextToSpeech.SUCCESS){
-                                    int result=mTts.setLanguage(Locale.US);
-                                    if(result==TextToSpeech.LANG_MISSING_DATA ||
-                                            result==TextToSpeech.LANG_NOT_SUPPORTED){
-                                        Log.e(TAG, "This Language is not supported");
-                                    }
-                                    else{
-                                        ConvertTextToSpeech(finalText);
-                                    }
-                                }
-                                else
-                                    Log.e("error", "Initilization Failed!");
-                            }
-                        });
-
-                    }
-                }
-            });
-        }
-
+        Button b2 = findViewById(R.id.speak_button2);
+        b2.setOnClickListener(v -> buttonClickImp(findViewById(R.id.theText2)));
     }
 
     @Override
@@ -69,14 +34,28 @@ public class Text2Speech extends Activity {
         super.onResume();
     }
 
-    private void ConvertTextToSpeech(String text) {
-        if(text==null||"".equals(text))
-        {
-            text = "Content not available";
-            mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-        }else {
-	    mTts.setSpeechRate(SPEACH_RATE);
-	    mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-	}
+    private void buttonClickImp(final EditText et) {
+        String etStr = et.getText().toString();
+        if (etStr.equals(""))
+            etStr = et.getHint().toString();
+        if (!etStr.equals("")) {
+            final String txt = etStr;
+            mTts = new TextToSpeech(mActivity, status -> {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTts.setLanguage(Locale.US);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+                        Log.e(TAG, "This Language is not supported");
+                    else {
+                        if("".equals(txt))
+                            mTts.speak("Content not available", TextToSpeech.QUEUE_FLUSH, null);
+                        else {
+                            mTts.setSpeechRate(SPEACH_RATE);
+                            mTts.speak(txt, TextToSpeech.QUEUE_FLUSH, null);
+                        }
+                    }
+                } else
+                    Log.e("error", "Initilization Failed!");
+            });
+        }
     }
 }
